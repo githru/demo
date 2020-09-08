e8521bfd329b78d7d7f3fa375c964b030cf1afd8
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-09-08 10:52:35 +0900

    added realm

---
+++ realm/precache-manifest.a0bbf3854a3dde091db83a095b1b2da8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45b3eb5f1a31bfd2eb84f5661b2da1b",
+    "url": "/realm-java-demo/index.html"
+  },
+  {
+    "revision": "bb97464c4c7ac508a9f1",
+    "url": "/realm-java-demo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3a4d4b95b37da3bfa763",
+    "url": "/realm-java-demo/static/css/main.745c7588.chunk.css"
+  },
+  {
+    "revision": "bb97464c4c7ac508a9f1",
+    "url": "/realm-java-demo/static/js/2.74024ec3.chunk.js"
+  },
+  {
+    "revision": "c5b430a6c97cb71df9a59518bea99107",
+    "url": "/realm-java-demo/static/js/2.74024ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4d4b95b37da3bfa763",
+    "url": "/realm-java-demo/static/js/main.f379aeec.chunk.js"
+  },
+  {
+    "revision": "e4880306f605f20091c0",
+    "url": "/realm-java-demo/static/js/runtime-main.eb6f78d6.js"
+  }
+]);
